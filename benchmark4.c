1ab0a0407b1382bef5e38a2fe7dfe3eeb8153dcd
David Green <adriansdacko@gmail.com>
2025-02-04 20:31:38 +0200

    Update benchmark4.c

--- benchmark4.c
+++ benchmark4.c
@@ -1,9 +1,9 @@
 #define CHARLIMIT 255
-#define N 5  // Max - 11, grafa virstones
+#define N 5  // Max - 11, grafa virstnes
 
 int main()
 {
-    char d[N][N] = {
+    volatile char d[N][N] = {
         {88, 11, 2, -44, 4},
         {9, -80, 1, 2, 13},
         {2, -1, 0, 1, 40},
@@ -11,11 +11,11 @@ int main()
         {4, 13, -2, 1, 90}
     };
     
-    for(unsigned char k = 0; k < N; k++)
+    for(volatile unsigned char k = 0; k < N; k++)
     {
-        for(unsigned char i = 0; i < N; i++)
+        for(volatile unsigned char i = 0; i < N; i++)
         {
-            for(unsigned char j = 0; j < N; j++)
+            for(volatile unsigned char j = 0; j < N; j++)
             {
                 if(d[i][k] != CHARLIMIT && d[k][j] != CHARLIMIT && d[i][k] + d[k][j] < d[i][j])
                 {
